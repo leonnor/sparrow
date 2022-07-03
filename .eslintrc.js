ef59ed791f6897a87fd1196c49970d03e9d4c863
Leonnor <958978379@qq.com>
2022-07-03 11:36:12 +0800

    feat: renderer

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    // 这里增加一行，用于支持后面的测试环境
     jest: true,
   },
   extends: ['airbnb-base'],
@@ -12,7 +11,15 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    // 这里添加一行规则把这条规则隐藏
+    // 关闭 eslint 的如下功能
     'import/prefer-default-export': 0,
+    'no-use-before-define': 0,
+    'no-shadow': 0,
+    'no-restricted-syntax': 0,
+    'no-return-assign': 0,
+    'no-param-reassign': 0,
+    'no-sequences': 0,
+    'no-loop-func': 0,
+    'no-nested-ternary': 0,
   },
 };
--- __tests__/utils.js
+++ __tests__/utils.js
@@ -3,3 +3,13 @@ export function createDiv() {
   document.body.appendChild(div);
   return div;
 }
+
+export function mount(parent, child) {
+  if (parent) {
+    parent.appendChild(child);
+  }
+}
+
+export function getAttributes(node, attributes) {
+  return attributes.reduce((total, cur) => (total[cur] = node.getAttribute(cur), total), {});
+}
